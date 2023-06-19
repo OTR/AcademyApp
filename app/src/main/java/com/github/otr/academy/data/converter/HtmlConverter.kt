e76e52534b89b93795c5dbc70003d1353ddf65c7
OTR <49165533+OTR@users.noreply.github.com>
2023-06-19 15:46:20 +0600

    [TEST] Create a Data Source with Track long descriptions to feed into further tests

--- app/src/main/java/com/github/otr/academy/data/converter/HtmlConverter.kt
+++ app/src/main/java/com/github/otr/academy/data/converter/HtmlConverter.kt
@@ -2,18 +2,15 @@ package com.github.otr.academy.data.converter
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import org.jsoup.nodes.Node
 
 /**
  *
  */
 object HtmlConverter {
 
-    fun convert(sourceHtml: String): String {
+    fun convertFromHtmlToText(sourceHtml: String): String {
         val parsed: Document = Jsoup.parse(sourceHtml)
 
-        //val body: Node = parsed.selectFirst("body")?.firstChild() ?: throw RuntimeException("")
         return  parsed.body().text()
     }
 
--- app/src/test/java/com/github/otr/academy/data/converter/HtmlConverterTest.kt
+++ app/src/test/java/com/github/otr/academy/data/converter/HtmlConverterTest.kt
@@ -3,11 +3,14 @@ package com.github.otr.academy.data.converter
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-
+/**
+ *
+ */
 class HtmlConverterTest {
 
     @Test
     fun testConverter() {
+        val listOfDescriptions = LongDescriptionDataSource.getListOfLongDescriptions()
         assertEquals(2, 2)
     }
 
