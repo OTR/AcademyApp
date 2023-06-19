c7d63b6e985bafea1bb469b52a25792d8c878f91
OTR <49165533+OTR@users.noreply.github.com>
2023-06-19 08:49:02 +0600

    [CHORE] Add dependency for JetPack navigation compose

--- app/src/main/java/com/github/otr/academy/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/com/github/otr/academy/presentation/main_screen/MainScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 import com.github.otr.academy.presentation.categories_screen.CategoriesScreen
 import com.github.otr.academy.presentation.login_screen.LoginScreen
@@ -15,6 +17,8 @@ import com.github.otr.academy.presentation.track_screen.TrackScreen
  */
 @Composable
 fun MainScreen() {
+
+    val navController: NavController = rememberNavController()
 
     val viewModel: MainViewModel = viewModel()
 
