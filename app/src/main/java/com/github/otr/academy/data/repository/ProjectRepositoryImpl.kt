035ca32ed45b7c10172b6cd30a8969582bd69684
OTR <49165533+OTR@users.noreply.github.com>
2023-06-20 17:30:06 +0600

    [CHORE] Add explicit names to lambda arguments

--- app/src/main/java/com/github/otr/academy/data/repository/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/github/otr/academy/data/repository/ProjectRepositoryImpl.kt
@@ -5,7 +5,8 @@ import com.github.otr.academy.data.network.ApiEndpoints
 import com.github.otr.academy.data.network.ApiFactory
 import com.github.otr.academy.domain.entitiy.Project
 import com.github.otr.academy.domain.repository.ProjectRepository
-import retrofit2.http.Body
+
+import retrofit2.Response
 
 /**
  *
@@ -15,7 +16,7 @@ class ProjectRepositoryImpl : ProjectRepository {
     private val apiClient: ApiEndpoints = ApiFactory.client
 
     override suspend fun getProjectById(projectId: Int): Project {
-        apiClient.getProjectById(projectId).let { response ->
+        apiClient.getProjectById(projectId).let { response: Response<ProjectContainerDTO> ->
             val body: ProjectContainerDTO? = response.body()
             if (response.isSuccessful && body != null) {
                 return body.projects.first { it.id == projectId }.mapToProject()
