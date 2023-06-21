2bb5e3ee43d1c0bfd3eced7746845adabdecd4ba
OTR <49165533+OTR@users.noreply.github.com>
2023-06-21 09:34:54 +0600

    [FEAT] Add instance of Room database to Activity

--- app/src/main/java/com/github/otr/academy/presentation/main_screen/MainActivity.kt
+++ app/src/main/java/com/github/otr/academy/presentation/main_screen/MainActivity.kt
@@ -3,6 +3,8 @@ package com.github.otr.academy.presentation.main_screen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.github.otr.academy.data.database.AppDatabase
+import com.github.otr.academy.presentation.AcademyApplication
 
 import com.github.otr.academy.presentation.theme.AcademyAppTheme
 
@@ -10,8 +12,14 @@ import com.github.otr.academy.presentation.theme.AcademyAppTheme
  *
  */
 class MainActivity : ComponentActivity() {
+
+    private lateinit var appDatabase: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // get our database instance from application
+        appDatabase = (application as AcademyApplication).database
+        //
         setContent {
             AcademyAppTheme {
                 MainScreen()
